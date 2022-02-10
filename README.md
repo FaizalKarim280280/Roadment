ecbe547e1088c274a042438d57db19a913089e95
Faizal Karim <faizal.karim.metro@gmail.com>
2022-02-10 18:31:48 +0530

    Added model training code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Roadment
+
+This is a project for the segmentation of roads from satellite images.
---
+++ src/imports.py
@@ -0,0 +1,19 @@
+import re
+import os
+import sys
+import numpy as np
+import cv2 as op
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tqdm import tqdm
+from os import listdir
+import pandas as pd
+from keras.layers import Dense, Dropout, Input, add, Conv2D, BatchNormalization, MaxPooling2D, Conv2DTranspose,Activation, Concatenate
+from tensorflow import keras
+from time import time
+from tqdm import tqdm
+from keras import backend as K
+# import segmentaton_models as sm
+
+def main():
+    pass
